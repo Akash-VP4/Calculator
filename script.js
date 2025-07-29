8fcc5e48a3e3d0e235a2344322ff86a6be3e883e
Akash VP <221047972+Akash-VP4@users.noreply.github.com>
2025-07-29 11:22:32 +0530

    change display

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function display(num) {
 const buttons = document.querySelectorAll("button");
 
 const operators = ["+", "-", "*", "/"];
-const valuess=["1","2","3","4","5","6","7","8","9","0"]
+const valuess = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 let number1 = "";
 let number2 = "";
@@ -34,6 +34,7 @@ let oper = "";
 let flag = 0;
 let dispContent = "";
 let prev = number1;
+let temp = "0";
 
 // on screan keyboard
 buttons.forEach((button) => {
@@ -66,91 +67,96 @@ buttons.forEach((button) => {
         number2 = "";
         oper = "";
         flag = 0;
+        display(number1);
+        temp = number1;
+        number1 = "";
       }
-      display(number1);
+      // display(number1);
     } else if (flag == 0 && !operators.includes(button.id)) {
       number1 += button.id;
       prev = number1;
+      temp="0"
+      display(number1);
     } else if (flag == 0) {
+      if(temp!="0"){
+        number1=temp
+      }
       flag = 1;
       oper = button.id;
       prev = number2;
     } else if (flag == 1 && operators.includes(button.id)) {
-      console.log(oper);
+      // console.log(oper);
       number1 = operate(parseFloat(number1), parseFloat(number2), oper);
       number2 = "";
       oper = button.id;
       prev = number2;
 
-      console.log(number1);
+      // console.log(number1);
     } else {
       number2 += button.id;
       prev = number2;
+      display(number2);
     }
 
-    display(number1 + oper + number2);
+    // display(number1 + oper + number2);
   });
 });
 
-
 // on external keybord
 document.addEventListener("keydown", function (event) {
- 
-   
-  
-  
-    if (event.key == ".") {
-      if (prev == number1 && !prev.includes(".")) {
-        number1 = number1 + ".";
-      } else if (prev == number2 && !prev.includes(".")) {
-        number2 = number2 + ".";
-      }
-    } else if (event.key == "Backspace") {
-      console.log(number1);
-      console.log(number1.length, number2.length, oper.length);
-      if (number2 != "" && number2.length != 0) {
-        number2 = number2.slice(0, -1);
-      } else if (oper.length != 0) {
-        oper = oper.slice(0, -1);
-      } else if (number1.length != 0) {
-        number1 = number1.slice(0, -1);
-      }
-    } else if (event.key == "clear") {
-      display("0");
-      number1 = "";
+  if (event.key == ".") {
+    if (prev == number1 && !prev.includes(".")) {
+      number1 = number1 + ".";
+    } else if (prev == number2 && !prev.includes(".")) {
+      number2 = number2 + ".";
+    }
+  } else if (event.key == "Backspace") {
+    console.log(number1);
+    console.log(number1.length, number2.length, oper.length);
+    if (number2 != "" && number2.length != 0) {
+      number2 = number2.slice(0, -1);
+    } else if (oper.length != 0) {
+      oper = oper.slice(0, -1);
+    } else if (number1.length != 0) {
+      number1 = number1.slice(0, -1);
+    }
+  } else if (event.key == "clear") {
+    display("0");
+    number1 = "";
+    number2 = "";
+    oper = "";
+    flag = 0;
+  } else if (event.key == "Enter") {
+    if (flag == 1) {
+      number1 = operate(parseFloat(number1), parseFloat(number2), oper);
       number2 = "";
       oper = "";
       flag = 0;
-    } else if (event.key == "Enter") {
-      if (flag == 1) {
-        number1 = operate(parseFloat(number1), parseFloat(number2), oper);
-        number2 = "";
-        oper = "";
-        flag = 0;
-      }
+      temp = number1;
       display(number1);
-    } else if (flag == 0 && !operators.includes(event.key) && valuess.includes(event.key)) {
-      number1 += event.key;
-      prev = number1;
-    } else if (flag == 0) {
-      flag = 1;
-      oper = event.key;
-      prev = number2;
-    } else if (flag == 1 && operators.includes(event.key)) {
-      console.log(oper);
-      number1 = operate(parseFloat(number1), parseFloat(number2), oper);
-      number2 = "";
-      oper = event.key;
-      prev = number2;
-
-      console.log(number1);
-    } else if (flag == 1 && valuess.includes(event.key)) {
-      number2 += event.key;
-      prev = number2;
+      number1 = "";
     }
+  } else if (flag == 0 && !operators.includes(event.key) && valuess.includes(event.key)) {
+    number1 += event.key;
+    prev = number1;
+    display(number1);
+  } else if (flag == 0) {
+    if (temp != "0") {
+      number1 = temp;
+    }
+    flag = 1;
+    oper = event.key;
+    prev = number2;
+  } else if (flag == 1 && operators.includes(event.key)) {
+    number1 = operate(parseFloat(number1), parseFloat(number2), oper);
+    number2 = "";
+    oper = event.key;
+    prev = number2;
+  } else if (flag == 1 && valuess.includes(event.key)) {
+    number2 += event.key;
+    prev = number2;
+    display(number2);
+  }
 
-    display(number1 + oper + number2);
-
-
+  // display(number1 + oper + number2);
 });
-
